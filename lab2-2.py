e57ed589a2c8311319ee8f82958e9284be53f553
mingyu0427 <garis2618@gamil.com>
2025-03-16 14:19:26 +0900

    firts commit

---
+++ lab2-2.py
@@ -0,0 +1,11 @@
+name = '손민규'
+print('나의 이름은 :', name)
+age = 22
+print('나의 나이는 :', age)
+height = 177
+print('나의 키는', height, 'cm 입니다.')
+sum = 10 + 20
+print('10 + 20 =', sum)
+
+mult = 10 * 20
+print('10 * 20 =', mult)
