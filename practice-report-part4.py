2167f353076a930aa54c00167718ea2ead8e78d0
mingyu0427 <garis2618@gmail.com>
2025-04-01 22:35:02 +0900

    first commit

---
+++ practice-report-part4.py
@@ -0,0 +1,98 @@
+#4.1for i in range(1, 10):
+ #      print("2 * {} = {}".format(i, (2*i)))
+
+#i = 0
+#while i < 10:
+ #   i = i + 1
+  #  if i >=10: break
+   # print("2*{} = {}".format(i, (2*i)))
+ 
+
+#4.3Python 
+ #  is 
+  # FUN! 
+   #Python 
+   #is 
+   #FUN! 
+   #Python 
+   #is 
+   #FUN! 
+   #Python 
+   #is 
+   #Python 
+   #is 
+   #Python 
+   #is 
+   #FUN! 
+   #Python 
+   #Python 
+   #Python 
+   #is 
+   #FUN!
+#4.5menu = ['헴버거', '치킨', '피자']
+   #b = '헴버거'
+   #c = '치킨'
+   #p = '피자'
+   #while menu not in [ 'b', 'c', 'p' ]:
+       #menu = input("메뉴를 다시 입력하세요:")
+
+   #if 'b' in menu:
+       #print("헴버거를 선택하였습니다")
+   #elif 'c' in menu:
+       #print("치킨을 선택하였습니다")
+   #elif 'p' in menu:
+       #print("피자를 선택하였습니다")
+   #else:
+       #print("메뉴를 다시 선택하세요") 
+    
+#4.7a = int(input("숫자를 입력하세요:"))
+   #for i in range(2, a):
+      #if a%i==0:
+         #print("{}는 소수가 아닙니다".format({a}))
+      #else:
+         #print("{}는 소수입니다".format({a}))
+#4.9a = int(input("숫자를 입력하세요:"))
+   #for i in range(1, a+1):
+       #a = (a*(a+1)*(2*a+1))/6
+
+   #print("결과는 {}입니다".format({a}))    
+#4.11depth = 0
+    #day = 0
+    #while True:
+        #depth = depth + 7
+        #day = day + 1
+        #print("day:{}달팽이의 위치 :{}미터".format(day, depth))
+        #if depth > 30:
+            #break
+        #depth = depth - 5
+        #print("우물을 탈출하는데{}일 걸렸습니다.".format(day))
+
+#4.13for i in range(1, 10):
+        #for j in range(1, 10):
+            #for k in range(1, 10):
+                #if i*100 + j*10 + k == i**3 + j**3 + k**3:
+                    #print("세 자리의 암스트롱 수:",i*100+j*10+k)
+  
+#4.15F = 500
+    #a = int(input("충전 또는 사용한 연료를 +/- 기호와 함꼐 입력하시오:"))
+    #if F+a < 50:
+        #print("연료가 10% 미만이니 충전하세요!")
+    #else:
+        #print("현재 탱크양은 {} 입니다".format(F+a))
+#4.17for i in range(1, 20001):
+        #s = 0
+        #s1 = 0
+        #for j in range(1,i):
+        #if i % j ==0:
+            #s = s + j
+        #for k in range(1,s):
+            #if s % k == 0:
+                #s1 = s1 + k
+        #if i == s1 and i !=s:
+            #print("{}의 친화수 {}'.format(i, s))
+
+                  
+
+    
+    
+     
