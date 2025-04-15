8764d0107a2f95ca693e5eb6883f6d4f0fcaec21
mingyu0427 <garis2618@gmail.com>
2025-04-15 23:20:51 +0900

    first commit

---
+++ practice-report-part9.py
@@ -0,0 +1,14 @@
+#9.5
+#with open("number1to10.txt", "r") as file:
+#    for line in file:
+#        print(line.strip())
+#9.9
+#points = []
+#with open("coord.txt", "r") as file:
+#    for line in file:
+#        x, y = map(int, line.strip().split())
+#        points.append((x, y))
+#points.sort()
+#with open("coord_sorted.txt", "w") as file:
+#    for x, y in points:
+#        file.write(f"{x} {y}\n")
